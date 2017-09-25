8e17ce5519d6dbc7e4fb494adfbbce6c62add081
Vasiliy Korchagin <vasiliy.korchagin@gmail.com>
2017-09-25 20:14:37 +0100

    Add new files to install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,10 @@
+#!/bin/sh
+FILES="dircolors.256dark .vimrc .zshrc.local"
+
 DIR=`dirname $0`
-ln -s $DIR/.vimrc ~/.vimrc
+for f in $FILES
+do
+	ln -bfs $DIR/$f ~/$f
+done
+
+ln -bfs $DIR/.tmux.conf ~/.byobu/.tmux.conf
